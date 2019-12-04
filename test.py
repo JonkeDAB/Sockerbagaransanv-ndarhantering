3936c54e32ba0e82277cf8d803d625c31c4ee49a
Teo Junevall <teo.junevall@learnet.se>
2019-12-04 09:47:16 +0100

    commandline printas

--- test.py
+++ test.py
@@ -1,25 +1,25 @@
 import os, csv, random, string
 
-def getFileLocation():
-    print('Skriv sökvägen till CSV filen.')
-    csvlocation = input("csv: ")
-    if os.path.isfile(csvlocation) == False:
-        print("Filen existerar inte, vänligen testa igen.")
-        getFileLocation()
-    elif os.path.isfile(csvlocation) == True:
-        print("Filen existerar.")
+# def getFileLocation():
+#     print('Skriv sökvägen till CSV filen.')
+#     csvlocation = input("csv: ")
+#     if os.path.isfile(csvlocation) == False:
+#         print("Filen existerar inte, vänligen testa igen.")
+#         getFileLocation()
+#     elif os.path.isfile(csvlocation) == True:
+#         print("Filen existerar.")
         
-        correctcsvlocation = csvlocation
-    else:
-        print("gg")
+#         correctcsvlocation = csvlocation
+#     else:
+#         print("gg")
     
-    return correctcsvlocation
+#     return correctcsvlocation
 
-print(getFileLocation())
+# print(getFileLocation())
 with open ('MOCK_DATA.csv', newline='') as csvfile:
     readFile = csv.DictReader(csvfile)
     for row in readFile:
-        print(f'{row['first_name']} with ID:  {row['id']}')
+        print(f'{row["first_name"]} with ID:  {row["id"]}')
 # chars = "abcdefghijklmnopqrstuvwxyz"
 # chars1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 # chars2 = "0123456789"
